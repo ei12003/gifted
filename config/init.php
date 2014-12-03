6082b629cc09092ab87417492d23e77ffc27ee4b
ei12003 <ei12003@fe.up.pt>
2014-12-03 11:04:46 +0000

    config updated

--- config/init.php
+++ config/init.php
@@ -1,9 +1,9 @@
 <?php
-	session_set_cookie_params(3600, '/~ldso');
+	
 	session_start();
 
-	$BASE_DIR = 'C:/Bitnami/wappstack-5.4.34-0/apps/demo/htdocs/';
-	$BASE_URL = '/demo/';
+	$BASE_DIR = 'C:/xampp/htdocs/';
+	$BASE_URL = '/gifted/';
 	$CURRENT_PAGE = 'home';
 
 	// DB //
@@ -13,27 +13,42 @@
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	//$conn->exec('SET SCHEMA \'public\'');
 
-	include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
+	include_once($BASE_DIR.$BASE_URL. 'lib/smarty/Smarty.class.php');
 
 	$smarty = new Smarty;
-	$smarty->template_dir = $BASE_DIR . 'templates/';
-	$smarty->compile_dir = $BASE_DIR . 'templates_c/';
+	$smarty->template_dir = $BASE_DIR.$BASE_URL . 'templates/';
+	$smarty->compile_dir = $BASE_DIR.$BASE_URL . 'templates_c/';
 	$smarty->assign('BASE_URL', $BASE_URL);
 	$smarty->assign('CURRENT_PAGE',$CURRENT_PAGE);
-
+if(isset($_SESSION['error_messages']))
 	$smarty->assign('ERROR_MESSAGES', $_SESSION['error_messages']);  
+if(isset($_SESSION['field_errors']))
 	$smarty->assign('FIELD_ERRORS', $_SESSION['field_errors']);
+if(isset($_SESSION['success_messages']))
 	$smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
+if(isset($_SESSION['form_values']))
 	$smarty->assign('FORM_VALUES', $_SESSION['form_values']);
+
+if(isset($_SESSION['username']))
 	$smarty->assign('USERNAME', $_SESSION['username']);
+else
+	$smarty->assign('USERNAME', array());
+
+if(isset($_SESSION['usertype']))
 	$smarty->assign('USERTYPE', $_SESSION['usertype']);
+else
+	$smarty->assign('usertype', array());
+
+if(isset($_SESSION['userid']))
 	$smarty->assign('USERID', $_SESSION['userid']);
+else
+	$smarty->assign('USERID', array());
 	
 	
 	//TOREMOVE
 	//for testing purposes
- 	$smarty->assign('USERNAME', "BOB");
-	$smarty->assign('USERTYPE', "teacher");
+ 	//$smarty->assign('USERNAME', "BOB");
+	//$smarty->assign('USERTYPE', "teacher");
 	////
 	
 	
@@ -41,4 +56,5 @@
 	unset($_SESSION['error_messages']);  
 	unset($_SESSION['field_errors']);
 	unset($_SESSION['form_values']);
+		
 ?>
